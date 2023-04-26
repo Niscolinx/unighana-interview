34889a2778f0d38664df0032509ac486233f2824
Igboanugwo Collins <munisco12@gmail.com>
2023-04-26 05:38:12 +0100

    working on fetching

--- src/components/HOC/ReactQuery.tsx
+++ src/components/HOC/ReactQuery.tsx
@@ -2,6 +2,9 @@
 
 import React from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+
 
 
 interface Props {
